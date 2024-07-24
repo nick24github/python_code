696f6cd1a46d862803b7ee6af9ced98484793972
nick24github <nikitarawanisdl@gmail.com>
2024-07-24 18:27:47 +0530

    python intervew questions uploaded

---
+++ Interview_Questions/PythonInterview.py
@@ -0,0 +1,72 @@
+# 4 part of python interview questions
+
+# 1) Mostly Asked Question (10)
+
+#  Q.1) What is diff b/w list and tuple ?
+# --------------------------------------------
+# LIST:
+# 1)list is mutable.
+# 2)list is a container to contain different type of objects and is used to iterate object.
+# 3)Syntax: list = ['a', 'b', 'c',1, 2,3]
+# 4)list iteration is slower.
+# 5)list consumes more memory.
+# 6)Operations like insertion, deletion, are better performed.
+
+# TUPLE:
+# 1)tuple is immutale.
+# 2)tuple is also same is  list but contains immutable objects.
+# 3)Syntax: tuple = ('a', 'b', 'c' ,1 , 2)
+# 4)tuple processing is faster than list.
+# 5)tuple consumes less memory.
+# 6)Elements can be accessed better.
+
+# Q.2)What is decorator. Explain it with an example.
+# ---------------------------------------------------
+# A Decorator is just a function that takes another function as an argument, add some kind of 
+# functionality and then returns another functions.
+# All of this without altering the source code of the original function that you passed in.
+# Example:-
+
+# Q.3)What is diff b/w list and dict (list comprehension and dict comprehension)
+# -------------------------------------------------------------------------------
+# LIST COMPREHENSION:
+# Syntax: [expression for item in iterable if conditional]
+# DICT COMPREHENSION:
+# Syntax: {key:value for(key,value) in iterable if condition}
+# Q.4)How memory managed in python ?
+# ------------------------------------
+# Private Heap Memory
+# Space Allocation
+# Python Memory Manager
+# Definition:
+# 1)Memory management in python involves a private heap containing all Python objects
+# and data structures. Interpretrer takes care of Python heap and that the programmer 
+# has no access to it.
+# 2)The allocation of heap space for Python objects is done by
+# Python memory manager. The core API of Python provides some tools for the 
+# programmer to code reliable and more robust program.
+# 3)Python also has a build-in garbage collector which recycles all the unused memory.
+# When an object is no longer referenced by the program, the heap space it occupies can 
+# be freed. The garbage collector determines objects which are no longer referenced by the
+# program frees the occupied memory and make it available to the heap space.
+# 4)The gc module defines functions to enable/disable garbage collector.
+# gc.enable() - Enables automatic garbage collection.
+# gc.disable() - Disables automatic garbage collection.
+
+# Q.5)What is difference between Generator & Iterator ?
+# ----------------------------------------------------
+# GENERATOR:
+# 1)Generators are iterators which can execute only once.
+# 2)Generator uses "yield" keyword.
+# 3)Generators are mostly used in loops to generate an iterator by returning all the values in the loop
+# without affecting the iteration of the loop.
+# 4)Every generator is an iterator.
+# ITERATOR:
+# 1)An iterator is an object which containes a countable number of values and it is used to iterate
+# over iterable objects like list, tuples, sets, etc.
+# 2)Iterators are used mostly to iterate or convert other objects to an iterator using iter() function. 
+# 3)Iterators uses iter() and next() function.
+# 4)Every iterator is not a generator.
+# 2) Commonly Asked Question (20)
+# 3) Tricky Questions (10)
+# 4) Advance Questions (12)
